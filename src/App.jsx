8f3e2a20e65f9eba91e194cac5f130b1f2d63dce
Siwar Saad <siwarsaad2000@gmail.com>
2026-05-19 14:52:42 +0200

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,19 +12,12 @@ import Signup from "./page/login_signup/Signup";
 import ForgotPassword from "./page/login_signup/ForgotPassword";
 import ResetPassword from "./page/login_signup/ResetPassword";
 
-import BasicFacts from "./page/BasicFacts";
-import History from "./page/History";
-import Geography from "./page/Geography";
-import DestinationsInfo from "./page/DestinationsInfo";
-import Activities from "./page/Activities";
-import Food from "./page/Food";
 import Packages from "./page/Packages";
+import Hotels from "./page/hotel/Hotels";
+import FlightComingSoon from "./page/flight/flightcomingsoon";
 
 import Admin from "./page/admin/Admin";
 import UserProfile from "./page/client/profile/UserProfile";
-import Hotels from "./page/hotel/Hotels";
-
-import FlightComingSoon from "./page/flight/flightcomingsoon";
 
 import ProtectedRoute from "./components/ProtectedRoute";
 
@@ -42,7 +35,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* PUBLIC ROUTES */}
+        {/* AUTH */}
         <Route
           path="/login"
           element={token && user ? <Navigate to="/" replace /> : <Login />}
@@ -69,17 +62,10 @@ function App() {
 
         {/* MAIN PAGES */}
         <Route path="/" element={<Home />} />
-
-        <Route path="/basic-facts" element={<BasicFacts />} />
-        <Route path="/history" element={<History />} />
-        <Route path="/geography" element={<Geography />} />
-        <Route path="/destinations-info" element={<DestinationsInfo />} />
-        <Route path="/activities" element={<Activities />} />
-        <Route path="/food" element={<Food />} />
         <Route path="/packages" element={<Packages />} />
         <Route path="/hotels" element={<Hotels />} />
 
-        {/* FLIGHT PAGE */}
+        {/* FLIGHT COMING SOON */}
         <Route path="/flight" element={<FlightComingSoon />} />
         <Route path="/flights" element={<FlightComingSoon />} />
 
