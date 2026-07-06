8a105078b652cbe6ccc6cfddbb5956cbdf9acd00
NesrineGhaddab <ghaddabnessrine@gmail.com>
2026-07-06 14:56:20 +0100

    target

--- vite.config.js
+++ vite.config.js
@@ -11,11 +11,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: ' https://egyptholidaytravel.com/api',
         changeOrigin: true
       },
       '/images': {
-        target: 'http://localhost:3000',
+        target: ' https://egyptholidaytravel.com/api',
         changeOrigin: true
       }
     }
